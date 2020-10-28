0102904b6b0f0338c8d68bbac16f6896dccb2950
xMalikeNx <68480258+xMalikeNx@users.noreply.github.com>
2020-10-28 12:28:12 +0300

    task_4 initial app configuration created (#31)
    * task_4 init done
    
    * task_4 webpack.config.js updated
    
    * task_4 rework after review

---
+++ webpack.config.ts
@@ -0,0 +1,64 @@
+import * as path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin'; // eslint-disable-line import/no-extraneous-dependencies
+
+module.exports = {
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 2,
+              modules: {
+                localIdentName: '[local]__[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: process.env.PORT || 9000,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.svg', '.png', '.jpg'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './build'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+  ],
+};
